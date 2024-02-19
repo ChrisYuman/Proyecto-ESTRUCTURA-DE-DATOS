8dd9d2ff3d2af445974d29e7be904d987ff67c9b
Chris <Chris@CHRISTOPHER>
2024-02-19 07:18:40 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto, ESTRUCTURA DE DATOS/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto__ESTRUCTURA_DE_DATOS
+{
+    public partial class Form1 : Form
+    {
+        private const string UsuarioValido = "admin";
+        private const string ContraseñaValida = "12345";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
