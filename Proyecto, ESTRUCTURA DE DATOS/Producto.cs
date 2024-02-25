d54548899b60ee8b8a1fb24349d3aa93797e2480
Mario <3943008660101@ingenieria.usac.edu.gt>
2024-02-25 12:45:04 -0600

    creacion de clases usuario y producto

---
+++ Proyecto, ESTRUCTURA DE DATOS/Producto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto__ESTRUCTURA_DE_DATOS
+{
+    public class Producto
+    {
+        public string Nombre {  get; set; }
+        public string Descripcion {  get; set; }
+        public double Precio { get; set; }
+        public int Cantidad_Disponible {  get; set; }
+        
+        public Producto(string nombre, string descripcion, double precio, int cantidad_disponible)
+        {
+            Nombre = nombre;
+            Descripcion = descripcion;
+            Precio = precio;
+            Cantidad_Disponible = cantidad_disponible;
+        }
+    }
+}
---
+++ Proyecto, ESTRUCTURA DE DATOS/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto__ESTRUCTURA_DE_DATOS
+{
+    public class Usuario
+    {
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public string Password { get; set; }
+
+        public Usuario(string nombre, string correo, string password)
+        {
+            Nombre = nombre;
+            Correo = correo;
+            Password = password;
+        }
+    }
+}
