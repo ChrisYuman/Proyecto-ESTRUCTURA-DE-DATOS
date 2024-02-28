58f20ebc51251e3194a551f50854ecf45351964c
Christopher Yuman <chrisyumanv@gmail.com>
2024-02-28 01:47:39 -0600

    Mas diseños

--- Proyecto, ESTRUCTURA DE DATOS/Pagina principal.Designer.cs
+++ Proyecto, ESTRUCTURA DE DATOS/Pagina principal.Designer.cs
@@ -28,58 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Pagina_principal));
             this.button1 = new System.Windows.Forms.Button();
             this.btnBuscar = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.Registrar = new System.Windows.Forms.ToolTip(this.components);
+            this.Buscar = new System.Windows.Forms.ToolTip(this.components);
+            this.CompraYventa = new System.Windows.Forms.ToolTip(this.components);
+            this.Actualizar = new System.Windows.Forms.ToolTip(this.components);
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(74, 86);
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Location = new System.Drawing.Point(167, 67);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(92, 28);
+            this.button1.Size = new System.Drawing.Size(198, 170);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Registrar productos";
+            this.Registrar.SetToolTip(this.button1, "Registrar");
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // btnBuscar
             // 
-            this.btnBuscar.Location = new System.Drawing.Point(279, 88);
+            this.btnBuscar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnBuscar.BackgroundImage")));
+            this.btnBuscar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnBuscar.Location = new System.Drawing.Point(534, 67);
             this.btnBuscar.Name = "btnBuscar";
-            this.btnBuscar.Size = new System.Drawing.Size(147, 24);
+            this.btnBuscar.Size = new System.Drawing.Size(159, 170);
             this.btnBuscar.TabIndex = 1;
-            this.btnBuscar.Text = "Buscar Productos";
+            this.Buscar.SetToolTip(this.btnBuscar, "Buscar");
             this.btnBuscar.UseVisualStyleBackColor = true;
             this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(74, 159);
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.Location = new System.Drawing.Point(167, 313);
             this.button2.Name = "button2";
             this.button2.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.button2.Size = new System.Drawing.Size(138, 27);
+            this.button2.Size = new System.Drawing.Size(198, 181);
             this.button2.TabIndex = 2;
-            this.button2.Text = "Compra y Venta";
+            this.CompraYventa.SetToolTip(this.button2, "Compra Y Venta");
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(293, 159);
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button3.Location = new System.Drawing.Point(534, 313);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(159, 170);
             this.button3.TabIndex = 3;
-            this.button3.Text = "Actualizar";
+            this.Actualizar.SetToolTip(this.button3, "Actualizar");
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Registrar
+            // 
+            this.Registrar.ToolTipTitle = "Registrar";
+            this.Registrar.Popup += new System.Windows.Forms.PopupEventHandler(this.toolTip1_Popup);
+            // 
+            // Buscar
+            // 
+            this.Buscar.ToolTipTitle = "Buscar";
+            // 
+            // CompraYventa
+            // 
+            this.CompraYventa.ToolTipTitle = "Compra Y Venta";
+            // 
+            // Actualizar
+            // 
+            this.Actualizar.ToolTipTitle = "Actualizar";
+            this.Actualizar.Popup += new System.Windows.Forms.PopupEventHandler(this.Actualizar_Popup);
             // 
             // Pagina_principal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(886, 550);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.btnBuscar);
@@ -97,5 +130,9 @@
         private System.Windows.Forms.Button btnBuscar;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ToolTip Registrar;
+        private System.Windows.Forms.ToolTip Buscar;
+        private System.Windows.Forms.ToolTip CompraYventa;
+        private System.Windows.Forms.ToolTip Actualizar;
     }
 }
--- Proyecto, ESTRUCTURA DE DATOS/RegistroForm.Designer.cs
+++ Proyecto, ESTRUCTURA DE DATOS/RegistroForm.Designer.cs
@@ -97,28 +97,34 @@
             // 
             // pictureBox1
             // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(12, 22);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(282, 77);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 8;
             this.pictureBox1.TabStop = false;
             // 
             // pictureBox2
             // 
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
             this.pictureBox2.Location = new System.Drawing.Point(12, 122);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(306, 75);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 9;
             this.pictureBox2.TabStop = false;
             // 
             // pictureBox3
             // 
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
             this.pictureBox3.Location = new System.Drawing.Point(12, 218);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(438, 87);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox3.TabIndex = 10;
             this.pictureBox3.TabStop = false;
             // 
@@ -127,7 +133,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.PeachPuff;
-            this.ClientSize = new System.Drawing.Size(823, 523);
+            this.ClientSize = new System.Drawing.Size(808, 523);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
